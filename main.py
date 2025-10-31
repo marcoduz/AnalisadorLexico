cf49bd654db407f98f943d1d46afa1a42beeeee4
CarlosGiovaneNN <carlosgiovane1289@gmail.com>
2025-10-31 16:02:11 -0300

    feat - commentando a glc

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from GLC import *
 from SLR_parser_generator import gerar_gramatica
 from SLR import *
 
-tokens = ["se", "foi"]
-gramaticas = [["S::=fA|aA|eA|iA", "A::=fA|aA|eA|iA|"]]
+tokens = ["id", "if", "(", ")", "{", "}", "else", "while", "return", ";", "=", "+", "-", "*", "/", "&", ">", "<", ">=", "<=", "==", "!=", "and", "or", "true", "false"]
+gramaticas = [["S::=$A", "A::=a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z|w|y|z"], ["S::=1A|2A|3A|4A|5A|6A|7A|8A|9A|0A", "A::=1A|2A|3A|4A|5A|6A|7A|8A|9A|0A|"],]
 fita = []
 ts = []
 
@@ -19,11 +19,23 @@ analisadorLexico(AFDetrministico, fita, ts)
 
 AFDetrministico.exibir_automato()
 
-teste = GLC([], [])
+#teste = GLC([], [])
 
-teste.criarRegrasPartirDoAFD(AFDetrministico)
+#teste.criarRegrasPartirDoAFD(AFDetrministico)
 
-x = gerar_gramatica(teste)
+#x = gerar_gramatica(teste)
 
-a = SLR(x, teste.naoTerminais)
-a.exibir()
+# a = SLR(x, teste.naoTerminais)
+# a.exibir()
+
+# BLOCO_CODIGO ::= CODIGO | CODIGO BLOCO_CODIGO
+# CODIGO_IF ::= if (BOOL) { BLOCO_CODIGO } FINAL_CODIGO_IF
+# FINAL_CODIGO_IF ::= else { BLOCO_CODIGO } | if (BOOL) { BLOCO_CODIGO } FINAL_CODIGO_IF |
+# BOOL ::= VAL_COMP OP_COMP VAL_COMP | true | false
+# OP_COMP ::= > | < | >= | <= | == | != | or | and
+# VAL_COMP ::= (BOOL) | VAL
+# CODIGO ::= SALVAR_VAR | CODIGO_IF
+# SALVAR_VAR ::= t_id = VAL EQ ;
+# EQ ::= OP VAL EQ |
+# OP ::= + | - | * | /
+# VAL ::= t_num
